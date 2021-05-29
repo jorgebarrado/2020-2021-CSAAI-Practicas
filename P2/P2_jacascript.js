9579402d6b8792725241b7c7507dee6291bb36dd
jorgebarrado <j.barrado.2016@alumnos.urjc.es>
2021-05-29 18:44:27 +0200

    muchos cambios

--- P2/P2_jacascript.js
+++ P2/P2_jacascript.js
@@ -4,80 +4,96 @@ console.log("Ejecutando JS...");
 //-- Elementos de la interfaz de la calculadora
 display = document.getElementById("display")
 display_2 = document.getElementById('display_2')
-suma = document.getElementById("suma")
-resta = document.getElementById("resta")
-multiplicacion = document.getElementById("multiplicacion")
-division = document.getElementById("division")
 igual = document.getElementById("igual")
 clear = document.getElementById("clear")
 borrar = document.getElementById("borrar")
 
 digito = document.getElementsByClassName("digito")
- 
-const max_digitos = 99;
-const max_calculos = 9;
-var n = 0;
-var cadena_historial = [];
+operador = document.getElementsByClassName("operador")
 
-function Open_Calculadora() {
-    document.getElementById('caja_todos_elementos').style.display = 'inline-block';
-    document.getElementById('caja_historial').style.display = 'inline-block';
-    elementoPadre = contenedor_abrir.parentNode
-    elementoPadre.removeChild(contenedor_abrir)
-  }
-  
+var op = 0;
+
+//-- Estados de la calculadora
+
+const ESTADO = {
+  OP0: 0,
+  OP1: 1,
+}
+
+let estado = ESTADO.OP0;
+
 // -- Insertar digito 
 for (i=0; i<digito.length; i++) {
 digito[i].onclick = (ev) => {
-  if (display.innerHTML == "0" ){
+  if (display.innerHTML == "0"){
       display.innerHTML = ev.target.value;
   }else{
     display.innerHTML += ev.target.value;
   }
+  if (estado == ESTADO.OP1){
+    op = op + 1;
+    display_2.innerHTML = eval(display.innerHTML);
+  }else{
+    display_2.innerHTML = eval(display.innerHTML);
+  }
 }
-}
-//-- Insertar simbolo de sumar
-suma.onclick = () => {
-  display.innerHTML += suma.value;
-}
-//-- Insertar simbolo de resta
-resta.onclick = () => {
-    display.innerHTML += resta.value;
 }
 
-//-- Insertar simbolo de multiplicacion
-multiplicacion.onclick = () => {
-    display.innerHTML += multiplicacion.value;
+// -- Insertar operador 
+for (i=0; i<operador.length; i++) {
+  operador[i].onclick = (ev) => {
+  
+    if (estado == ESTADO.OP0){
+      display_2.innerHTML =  display.innerHTML ;
+        display.innerHTML += ev.target.value;
+        estado = ESTADO.OP1;
+    }else{
+      display_2.innerHTML = 'NO MAS SIGNOS';
+    }
   }
-  //-- Insertar simbolo de division
-  division.onclick = () => {
-      display.innerHTML += division.value;
   }
+  
 
 borrar.onclick = () => {
- 
-    document.getElementById('error_maximo_digitios').style.display = 'none';
     let cadena = []
-  
     console.log(display.innerHTML.length);
-  
     if(display.innerHTML.length == 1){
       display.innerHTML = '0';
     }else{
       for (i=0; i<display.innerHTML.length-1; i++) {
         cadena += display.innerHTML[i];
       }
-      display.innerHTML  = cadena;
+      if (estado = ESTADO.OP1){
+        if (op == 0){
+          estado = ESTADO.OP0;
+            
+        }else{
+          op = op - 1; 
+        }
+         display.innerHTML  = cadena;
+      }else{ 
+        display.innerHTML  = cadena;
+      }
+      
     }
+    display_2.innerHTML = eval(display.innerHTML);
   }
 
 //-- Evaluar la expresion
 igual.onclick = () => {
     display.innerHTML = eval(display.innerHTML);
+    estado = ESTADO.OP0;
+    op = 0;
+    display_2.innerHTML = eval(display.innerHTML);
+
+    
   }
 //-- Poner a cero la expresion
 clear.onclick = () => {
   display.innerHTML = '0';
+  estado = ESTADO.OP0;
+  
+  op = 0;
 }
 
 
